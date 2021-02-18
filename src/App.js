369fcee9fba9bbea7858910e99e3f469112650ad
Haya <hm.almadhi@gmail.com>
2021-02-18 09:28:08 +0300

    deploy websit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Contact from './Contact'
+import Home from './Home'
+import Procedures from './Procedures'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const proceduresArray = [
+  'General Checkups',
+  'Teeth Cleaning',
+  'Cavity Screenings',
+  'Cavity Fillings',
+  'Chipped Tooth Fixings',
+  'Tooth Removal',
+  'Root Canals']
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>{' '}
+          <Link to="/procedures">Procedures</Link> {' '}
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <div>
+          {/* at this exact rout="/" */}
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/procedures" component={() => <Procedures procedures={proceduresArray} />}></Route>
+          <Route path="/contact" component={Contact}></Route>
+
+
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Dentist's Website</h1>
+                <p>Welcom to my Dentist's Website </p>
+            </div>
+        )
+    }
+}
